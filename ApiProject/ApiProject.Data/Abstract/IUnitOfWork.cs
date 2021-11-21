917de003e8083e4ec179d34a041cf4209c5ae5e2
bilgekara <karabilgenur@gmail.com>
2021-11-21 18:14:57 +0300

    unitOfWork

---
+++ ApiProject/ApiProject.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Data.Abstract
+{
+    /* contextimizi dispose ediyor olacaz, garbage collectora yardımcı olacaz
+    *  tüm repositoryleri bir yerden yönetmek amaçlı bu yapıyı kuruyoruz
+    *  ef'ye bagimli degiliz */
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        //resimlere erişmek amaçlı get
+        IPhotoRepository Photos { get; } // unitofwork.Photos
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; } // _unitOfWork.Comments.AddAsync();
+        // _unitOfWork.Comments.AddAsync(comment);
+        //_unitOfWork.Users.AddAsync(user);
+        //_unitOfWork.SaveAsync();
+        Task<int> SaveAsync();
+    }
+}
---
+++ ApiProject/ApiProject.Data/Concrete/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using ApiProject.Data.Abstract;
+using ApiProject.Data.Concrete.EntityFramework.Contexts;
+using ApiProject.Data.Concrete.EntityFramework.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Data.Concrete
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        // cunku repositoryler constructor icerisinde bizden bir db context nesnesi istiyordu, projede kullandıgımız contexti burda onlara veriyor olacaz
+        private readonly ApiProjectContext _context;
+        //degerleri readonly yapmıyoruz cunku new'lencekler
+        private EfPhotoRepository _photoRepository;
+        private EfCommentRepository _commentRepository;
+        private EfRoleRepository _roleRepository;
+        private EfUserRepository _userRepository;
+
+        public UnitOfWork(ApiProjectContext context)
+        {
+            _context = context;
+        }
+
+        /* repositorylerin somut hallerine ihtiyac var ->
+         * bir interface newlenemez normal interface'i return etmemiz mümkün değil
+         * bu interfacelerin somut hallerini entegre ediyoruz
+         */
+        public IPhotoRepository Photos => _photoRepository ?? new EfPhotoRepository(_context);
+
+        public ICommentRepository Comments => _commentRepository ?? new EfCommentRepository(_context);
+
+        public IRoleRepository Roles => _roleRepository ?? new EfRoleRepository(_context);
+
+        public IUserRepository Users => _userRepository ?? new EfUserRepository(_context);
+
+        // dispose metodunun async hali c# 8 ile geldi
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
