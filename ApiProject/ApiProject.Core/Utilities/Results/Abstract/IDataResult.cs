70a625303d906b692311561e93fe397fc4b55c4a
bilgekara <karabilgenur@gmail.com>
2021-11-22 11:02:48 +0300

    result added

---
+++ ApiProject/ApiProject.Core/Utilities/Results/Abstract/IDataResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Core.Utilities.Results.Abstract
+{
+    // data result sayesinde resultlar icinde verileri de tasiyabiliyor olacaz
+    // out T : veriyi hem tek basına hem de liste olarak ekleyebiliriz
+    public interface IDataResult<out T> : IResult
+    {
+        public T Data { get; } // new DataResult<Category>(ResultStatus.Success,category);
+                               // new DataResult<IList<Category>>(ResultStatus.Success, categoryList);
+    }
+}
---
+++ ApiProject/ApiProject.Core/Utilities/Results/Concrete/DataResult.cs
@@ -0,0 +1,38 @@
+﻿using ApiProject.Core.Utilities.Results.Abstract;
+using ApiProject.Core.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Core.Utilities.Results.Concrete
+{
+    public class DataResult<T> : IDataResult<T>
+    {
+        public DataResult(ResultStatus resultStatus, T data)
+        {
+            ResultStatus = resultStatus;
+            Data = data;
+        }
+        public DataResult(ResultStatus resultStatus, string message, T data)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Data = data;
+        }
+        public DataResult(ResultStatus resultStatus, string message, T data, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Data = data;
+            Exception = exception;
+        }
+
+        // get oldugu icin constructor ile verileri aliyoruz
+        public ResultStatus ResultStatus { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+        public T Data { get; }
+    }
+}
