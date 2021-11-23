2e1fffb1fe8e737788937e8fc77de12f0c00b1fb
bilgekara <karabilgenur@gmail.com>
2021-11-23 10:53:38 +0300

    business extensions

--- ApiProject/ApiProject.API/Startup.cs
+++ ApiProject/ApiProject.API/Startup.cs
@@ -1,3 +1,4 @@
+using ApiProject.Services.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,7 +26,7 @@ namespace ApiProject.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.LoadMyServices();
             services.AddControllers();
         }
 
---
+++ ApiProject/ApiProject.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using ApiProject.Data.Abstract;
+using ApiProject.Data.Concrete;
+using ApiProject.Data.Concrete.EntityFramework.Contexts;
+using ApiProject.Services.Abstract;
+using ApiProject.Services.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<ApiProjectContext>();
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>(); // biri senden IunitOfWork isterse sen ona UnitOfWork ver
+            serviceCollection.AddScoped<IPhotoService, PhotoManager>();
+            return serviceCollection;
+        }
+    }
+}
